aba5ed116ee63b824ae22f014e433ef9b3b8c62e
servanvetes <105884012+servanvetes@users.noreply.github.com>
2022-10-24 00:38:56 +0300

    Generic Interface

--- Entities/Repository/IGenericRepository.cs
+++ Entities/Repository/IGenericRepository.cs
@@ -27,17 +27,6 @@ namespace Entities.Repository
 
         IQueryable<T> GetWhere(Expression<Func<T, bool>>? predicate);
 
-        ///
-        ValueTask<T> GetByIdAsync(int id);
-        Task<IEnumerable<T>> GetAllAsync();
-        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
-        Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
-
-
-     
-      //  Task AddAsync(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities);
-        
-        void RemoveRange(IEnumerable<T> entities);
+       
     }
 }
